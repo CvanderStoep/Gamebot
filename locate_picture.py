8fedc157ec7506648c5b1dafefc507a981b8be19
carlo <carlo.vanderstoep@gmail.com>
2020-11-21 21:10:22 +0100

    find Lexy

--- locate_picture.py
+++ locate_picture.py
@@ -1,16 +1,15 @@
 """""
 Locates an image on the screen and moves the mouse to that location
-Can be usefull in games to locate objects and perform an action; for example mouseclick
+Can be useful in games to locate objects and perform an action; for example mouseclick
 The picture has to be in .png format
-The picture has to be in 100% format (no zoom in / zoom out
+The picture has to be in 100% format (no zoom in / zoom out)
 """
 
 import pyautogui
 import time
 
-
 while True:
-    start = pyautogui.locateCenterOnScreen('github.png')  # If the file is not a png file it will not work
+    start = pyautogui.locateCenterOnScreen('lexy.png')  # If the file is not a png file it will not work
     print(start)
     pyautogui.moveTo(start)  # Moves the mouse to the coordinates of the image
-    time.sleep(5) # sleep for 5 seconds to allow the user to close picture?
+    time.sleep(5)  # sleep for 5 seconds to allow the user to close picture?
